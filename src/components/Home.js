ac085003c36b5999b16e999f9d4343f027a7550f
Arturo Quiroz <r2inlondon@gmail.com>
2023-03-24 18:46:13 +0000

    feat : img compressed and splashScreenLoader

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,12 @@ import { useState } from "react";
 import LoginPage from "./LoginPage";
 import ApplicationForm from "./app-form/ApplicationForm";
 import Dashboard from "./dashboard/Dashboard";
+import SplashScreenLoader from "./loader/SplashScreenLoader";
+import bgImage from "../images/alevision-co-7ojyp-IXW7w-unsplash-3.jpg";
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [removeLoader, setRemoveLoader] = useState("");
   const [showComponent, setShowComponent] = useState({
     login: true,
     applicationForm: false,
@@ -15,15 +19,35 @@ const Home = () => {
     comp: showComponent,
   };
 
+  const handleImageLoad = () => {
+    setRemoveLoader("fadeOut");
+  };
+
   return (
-    <div className="bg-small">
-      {showComponent.login && <LoginPage componentsObject={componentsObject} />}
-      {showComponent.applicationForm && (
-        <ApplicationForm componentsObject={componentsObject} />
+    <div className="home-bg">
+      <img
+        src={bgImage}
+        alt=""
+        onLoad={() => handleImageLoad()}
+        className="main-bg"
+      />
+
+      {isLoading && (
+        <SplashScreenLoader setIsLoading={setIsLoading} anim={removeLoader} />
       )}
-      {showComponent.dashboard && (
-        <Dashboard componentsObject={componentsObject} />
-      )}
+      <div className="app-body">
+        <div className="bg-small">
+          {showComponent.login && (
+            <LoginPage componentsObject={componentsObject} />
+          )}
+          {showComponent.applicationForm && (
+            <ApplicationForm componentsObject={componentsObject} />
+          )}
+          {showComponent.dashboard && (
+            <Dashboard componentsObject={componentsObject} />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
